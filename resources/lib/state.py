123c25ea76a4f48a707dc39a8c9add78a8d5ad0b
Rick164 <RickDB@gmail.com>
2016-12-08 18:10:33 +0100

    Refresh rate is now automatically detected
    Since Kodi V17 it's possible to read out the refresh out from the video file playing, this will fix issues with video playback and make smoothing much more reliable.

--- resources/lib/state.py
+++ resources/lib/state.py
@@ -21,6 +21,7 @@
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 	THE SOFTWARE.
 '''
+import math
 import xbmc
 import xbmcaddon
 
@@ -28,157 +29,168 @@ from hyperion.Hyperion import Hyperion
 from misc import log
 from misc import notify
 
+
 class DisconnectedState:
-	'''
-	Default state class when disconnected from the Hyperion server
-	'''
+  '''
+  Default state class when disconnected from the Hyperion server
+  '''
 
-	def __init__(self, settings):
-		'''Constructor
-			- settings: Settings structure
-		'''
-		log("Entering disconnected state")
-		self.__settings = settings
+  def __init__(self, settings):
+    '''Constructor
+      - settings: Settings structure
+    '''
+    log("Entering disconnected state")
+    self.__settings = settings
 
-	def execute(self):
-		'''Execute the state
-			- return: The new state to execute
-		'''
-		# check if we are enabled
-		if not self.__settings.grabbing():
-			xbmc.sleep(500)
-			return self
+  def execute(self):
+    '''Execute the state
+      - return: The new state to execute
+    '''
+    # check if we are enabled
+    if not self.__settings.grabbing():
+      xbmc.sleep(500)
+      return self
 
-		# we are enabled and want to advance to the connected state
-		try:
-			nextState = ConnectedState(self.__settings)
-			return nextState
-		except Exception, e:
-			# unable to connect. notify and go to the error state
-			if self.__settings.showErrorMessage:
-				notify(xbmcaddon.Addon().getLocalizedString(32100))
-				self.__settings.showErrorMessage = False
+    # we are enabled and want to advance to the connected state
+    try:
+      nextState = ConnectedState(self.__settings)
+      return nextState
+    except Exception, e:
+      # unable to connect. notify and go to the error state
+      if self.__settings.showErrorMessage:
+        notify(xbmcaddon.Addon().getLocalizedString(32100))
+        self.__settings.showErrorMessage = False
 
-			# continue in the error state
-			return ErrorState(self.__settings)
+      # continue in the error state
+      return ErrorState(self.__settings)
+
 
 class ConnectedState:
-	'''
-	State class when connected to Hyperion and grabbing video
-	'''
+  '''
+  State class when connected to Hyperion and grabbing video
+  '''
 
-	def __init__(self, settings):
-		'''Constructor
-			- settings: Settings structure
-		'''
-		log("Entering connected state")
+  def __init__(self, settings):
+    '''Constructor
+      - settings: Settings structure
+    '''
+    log("Entering connected state")
 
-		self.__settings = settings
-		self.__hyperion = None
-		self.__capture = None
-		self.__captureState = None
-		self.__data = None
-		self.__useLegacyApi = True
+    self.__settings = settings
+    self.__hyperion = None
+    self.__capture = None
+    self.__captureState = None
+    self.__data = None
+    self.__useLegacyApi = True
 
-		# try to connect to hyperion
-		self.__hyperion = Hyperion(self.__settings.address, self.__settings.port)
+    # try to connect to hyperion
+    self.__hyperion = Hyperion(self.__settings.address, self.__settings.port)
 
-		# create the capture object
-		self.__capture = xbmc.RenderCapture()
-		self.__capture.capture(self.__settings.capture_width, self.__settings.capture_height)
+    # create the capture object
+    self.__capture = xbmc.RenderCapture()
+    self.__capture.capture(self.__settings.capture_width, self.__settings.capture_height)
 
-	def __del__(self):
-		'''Destructor
-		'''
-		del self.__hyperion
-		del self.__capture
-		del self.__captureState
-		del self.__data
-		del self.__useLegacyApi
+  def __del__(self):
+    '''Destructor
+    '''
+    del self.__hyperion
+    del self.__capture
+    del self.__captureState
+    del self.__data
+    del self.__useLegacyApi
 
-	def execute(self):
-		'''Execute the state
-			- return: The new state to execute
-		'''
-		# check if we still need to grab
-		if not self.__settings.grabbing():
-			# return to the disconnected state
-			return DisconnectedState(self.__settings)
+  def execute(self):
+    '''Execute the state
+      - return: The new state to execute
+    '''
+    # check if we still need to grab
+    if not self.__settings.grabbing():
+      # return to the disconnected state
+      return DisconnectedState(self.__settings)
 
-		# check the xbmc API Version
-		try:
-			self.__capture.getCaptureState()
-		except:
-			self.__useLegacyApi = False
+    # check the xbmc API Version
+    try:
+      self.__capture.getCaptureState()
+    except:
+      self.__useLegacyApi = False
 
-		# capture an image
-		startReadOut = False
-		if self.__useLegacyApi:
-			self.__capture.waitForCaptureStateChangeEvent(200)
-			self.__captureState = self.__capture.getCaptureState()
-			if self.__captureState == xbmc.CAPTURE_STATE_DONE:
-				startReadOut = True
-		else:
-			self.__data = self.__capture.getImage()
-			if len(self.__data) > 0:
-				startReadOut = True
+    # capture an image
+    startReadOut = False
+    if self.__useLegacyApi:
+      self.__capture.waitForCaptureStateChangeEvent(200)
+      self.__captureState = self.__capture.getCaptureState()
+      if self.__captureState == xbmc.CAPTURE_STATE_DONE:
+        startReadOut = True
+    else:
+      self.__data = self.__capture.getImage()
+      if len(self.__data) > 0:
+        startReadOut = True
 
-		if startReadOut:
-			if self.__useLegacyApi:
-				self.__data = self.__capture.getImage()
+    if startReadOut:
+      if self.__useLegacyApi:
+        self.__data = self.__capture.getImage()
 
-			# retrieve image data and reformat into rgb format
-			if self.__capture.getImageFormat() == 'ARGB':
-				del self.__data[0::4]
-			elif self.__capture.getImageFormat() == 'BGRA':
-				del self.__data[3::4]
-				self.__data[0::3], self.__data[2::3] = self.__data[2::3], self.__data[0::3]
+      # retrieve image data and reformat into rgb format
+      if self.__capture.getImageFormat() == 'ARGB':
+        del self.__data[0::4]
+      elif self.__capture.getImageFormat() == 'BGRA':
+        del self.__data[3::4]
+        self.__data[0::3], self.__data[2::3] = self.__data[2::3], self.__data[0::3]
 
-			try:
-				#send image to hyperion
-				self.__hyperion.sendImage(self.__capture.getWidth(), self.__capture.getHeight(), str(self.__data), self.__settings.priority, 500)
-			except Exception, e:
-				# unable to send image. notify and go to the error state
-				notify(xbmcaddon.Addon().getLocalizedString(32101))
-				return ErrorState(self.__settings)
+      try:
+        # send image to hyperion
+        self.__hyperion.sendImage(self.__capture.getWidth(), self.__capture.getHeight(), str(self.__data),
+                                  self.__settings.priority, 500)
+      except Exception, e:
+        # unable to send image. notify and go to the error state
+        notify(xbmcaddon.Addon().getLocalizedString(32101))
+        return ErrorState(self.__settings)
 
-		if self.__useLegacyApi:
-			if self.__captureState != xbmc.CAPTURE_STATE_WORKING:
-				#the current capture is processed or it has failed, we request a new one
-				self.__capture.capture(self.__settings.capture_width, self.__settings.capture_height)
+    if self.__useLegacyApi:
+      if self.__captureState != xbmc.CAPTURE_STATE_WORKING:
+        # the current capture is processed or it has failed, we request a new one
+        self.__capture.capture(self.__settings.capture_width, self.__settings.capture_height)
 
-		#limit the maximum number of frames sent to hyperion
-		xbmc.sleep( int(1. / self.__settings.framerate * 1000) )
+        # limit the maximum number of frames sent to hyperion
+    videofps = self.__settings.framerate
+    if (xbmc.Player().isPlaying()):
+      try:
+        videofps = math.ceil(float(xbmc.getInfoLabel('Player.Process(VideoFPS)')))
+      except ValueError:
+        pass
+    sleeptime = int(1. / videofps * 1000)
+    xbmc.sleep(sleeptime)
+    #xbmc.log('Hyperion sleeping for (ms): %d :: %.3f' % (sleeptime, videofps))
 
-		return self
+    return self
 
 class ErrorState:
-	'''
-	State class which is activated upon an error
-	'''
+  '''
+  State class which is activated upon an error
+  '''
 
-	def __init__(self, settings):
-		'''Constructor
-			- settings: Settings structure
-		'''
-		log("Entering error state")
-		self.__settings = settings
+  def __init__(self, settings):
+    '''Constructor
+      - settings: Settings structure
+    '''
+    log("Entering error state")
+    self.__settings = settings
 
-	def execute(self):
-		'''Execute the state
-			- return: The new state to execute
-		'''
-		# take note of the current revision of the settings
-		rev = self.__settings.rev
+  def execute(self):
+    '''Execute the state
+      - return: The new state to execute
+    '''
+    # take note of the current revision of the settings
+    rev = self.__settings.rev
 
-		#stay in error state for the specified timeout or until the settings have been changed
-		i = 0
-		while (i < self.__settings.timeout) and (rev == self.__settings.rev):
-			if self.__settings.abort:
-				return self
-			else:
-				xbmc.sleep(1000)
-			i += 1
+    # stay in error state for the specified timeout or until the settings have been changed
+    i = 0
+    while (i < self.__settings.timeout) and (rev == self.__settings.rev):
+      if self.__settings.abort:
+        return self
+      else:
+        xbmc.sleep(1000)
+      i += 1
 
-		# continue in the disconnected state
-		return DisconnectedState(self.__settings)
+    # continue in the disconnected state
+    return DisconnectedState(self.__settings)
